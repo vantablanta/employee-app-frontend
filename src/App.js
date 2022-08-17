ef31c94e2beb13fcae08b127dc2f861d0821405f
ckamande@hotmail <ckamande@hotmail.com>
2022-08-17 13:14:27 +0300

    pop up

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import AddForm from "./Components/AddForm";
 import Employee from "./Components/Employee";
 import Nav from "./Components//Menus/Nav";
 
+import Popup from "./Components/PopUp";
+
 function App() {
   return (
     <div className="container">
         <Nav/>
         <AddForm/>
         <Employee/>
+        <Popup/>
+
+        
+
     </div>
   );
 }
